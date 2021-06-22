71b100081b739a5a97b55f2531c99126cffd1621
Arun Mutharasu <55704859+CSharpDesignPro@users.noreply.github.com>
2021-06-22 21:59:48 +0530

    Add files via upload

---
+++ Circular ProgressBar_UC.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace Circular_ProgressBar
+{
+    /// <summary>
+    /// Interaction logic for Circular_ProgressBar_UC.xaml
+    /// </summary>
+    public partial class Circular_ProgressBar_UC : UserControl
+    {
+        public Circular_ProgressBar_UC()
+        {
+            InitializeComponent();
+        }
+
+        private void Storyboard_Completed(object sender, EventArgs e)
+        {
+            if(Application.Current.MainWindow is MainWindow mainWindow)
+            {
+                mainWindow.Start_Btn.IsChecked = false;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+
+namespace Circular_ProgressBar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Start / Stop Timer
+
+        DispatcherTimer _timer = new DispatcherTimer();
+        int counter = 0;
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            TimerLabel.Text = counter.ToString();
+
+            if(counter == 100)
+            {
+                _timer.Stop();
+                TimerLabel.Text = "0".ToString();
+            }
+
+        }
+
+        private void StartTimer()
+        {
+            cpb_uc.Visibility = Visibility.Visible;
+
+            if(counter > 0)
+            {
+                _timer.Tick -= timer_Tick;
+                counter = 0;
+            }
+
+            _timer.Interval = TimeSpan.FromMilliseconds(188);
+            _timer.Tick += timer_Tick;
+            _timer.Start();
+        }
+
+        private void StopTimer()
+        {
+            if(counter > 0)
+            {
+                _timer.Tick -= timer_Tick;
+                counter = 0;
+            }
+
+            _timer.Stop();
+            cpb_uc.Visibility = Visibility.Collapsed;
+            TimerLabel.Text = "0".ToString();
+        }
+
+        private void StartAnimation()
+        {
+            ((Storyboard)cpb_uc.Resources["ProgressBarAnimation"]).Begin();
+        }
+
+        private void StopAnimation()
+        {
+            ((Storyboard)cpb_uc.Resources["ProgressBarAnimation"]).Stop();
+        }
+
+        private void Start_Btn_Checked(object sender, RoutedEventArgs e)
+        {
+            StartTimer();
+            StartAnimation();
+        }
+
+        private void Start_Btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            StopTimer();
+            StopAnimation();
+        }
+
+        private void Uncheck_Stop(object sender, RoutedEventArgs e)
+        {
+            Start_Btn.IsChecked = false;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
